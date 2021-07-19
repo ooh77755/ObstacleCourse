f42a6ede2ec9f5b2b5a2de1cf54c29cb7a76cabb
Alan Stewart <alan.stewart@chi.ac.uk>
2021-07-19 16:50:17 +0100

    Added new 'ObjectHit' script, inc. 'OnCollisionEnter()' method containing 'Debug.log' code

---
+++ Assets/Scripts/ObjectHit.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectHit : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("Bumped into wall.");
+    }
+}
